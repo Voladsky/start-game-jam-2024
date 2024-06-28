af6de43c97b0151a1c164abd200fd0bdf42e6485
TheBogdichHD <nottoowellconstructed@gmail.com>
2024-06-28 16:42:26 +0300

    add tile_map an filed_tile

--- player/player.gd
+++ player/player.gd
@@ -4,11 +4,10 @@ extends CharacterBody2D
 
 var direction: Vector2 = Vector2.ZERO
 
-func _process(delta):
+func _process(_delta):
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
 
 func _physics_process(_delta):
 	velocity = direction * speed
 	move_and_slide()
-	
