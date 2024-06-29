6cb53101c14bd02622647d77c0ef05c99f219f9b
BurnedTuner <ainol0912@gmail.com>
2024-06-29 14:01:55 +0300

    PlayerIdle Animation

--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 @export var speed: int = 100
+@onready var animation = $AnimationPlayer
 
 var direction: Vector2 = Vector2.ZERO
 
 func _process(_delta):
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-
+	animation.play("Player_Idle")
 
 func _physics_process(_delta):
 	velocity = direction * speed
