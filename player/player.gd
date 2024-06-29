c82a779a517815c1fba6a42aaf2b1a661f6b8ea4
BurnedTuner <ainol0912@gmail.com>
2024-06-29 14:46:03 +0300

    Update player.gd

--- player/player.gd
+++ player/player.gd
@@ -10,12 +10,14 @@ func _process(_delta):
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if(direction.length() > 0):
 		animation.play("Player_Walk")
+		animation.speed_scale = 4
 		if(direction.x < 0):
 			GFX.scale.x = 1
 		else:
 			GFX.scale.x = -1
 	else:
 		animation.play("Player_Idle")
+		animation.speed_scale = 1
 
 func _physics_process(_delta):
 	velocity = direction * speed
