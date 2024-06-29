d1336933d26da9c541e11ddcc06a83860282130a
Владислав Крылов <krylov@sfedu.ru>
2024-06-29 11:02:29 +0300

    Added resource bar, plant generator (washing machine) interface

--- quote/quote.gd
+++ quote/quote.gd
@@ -1,25 +1,18 @@
 extends Node2D
 
 var quote_potato_amount = 30
-var current_potato_amount = 0
-
-@onready var label = $Label
 
 func compare_to_quote():
-	return quote_potato_amount <= current_potato_amount
+	return quote_potato_amount <= plant_amount.potato_amount
 
 func collect_potato():
-	current_potato_amount += 1
+	plant_amount.potato_amount += 1
 	
 func take_quote():
 	if !compare_to_quote():
 		get_tree().quit()
-	current_potato_amount -= quote_potato_amount
+	plant_amount.potato_amount -= quote_potato_amount
 	quote_potato_amount *= 2
-
-func _process(delta):
-	$Label.text = "%s/%s" % [current_potato_amount, quote_potato_amount]
-
 
 func _unhandled_key_input(event):
 	if event is InputEventKey:
