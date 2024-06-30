c5f8d10e3478bf168a461d040179a1009158d27e
Владислав Крылов <krylov@sfedu.ru>
2024-06-30 19:44:01 +0300

    Update plant_generator.gd

--- plant_generator/plant_generator.gd
+++ plant_generator/plant_generator.gd
@@ -20,12 +20,12 @@ func _on_body_exited(_body):
 
 
 func _on_cattail_pressed():
-	$Buy.play()
 	if GameManager.change_potato_amount(-cattail_cost):
+		$Buy.play()
 		GameManager.change_cattail_amount(1)
 
 
 func _on_spider_plant_pressed():
-	$Buy.play()
 	if GameManager.change_potato_amount(-spiderplant_cost):
+		$Buy.play()
 		GameManager.change_spiderplant_amount(1)
