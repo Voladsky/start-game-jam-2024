cafaee97ac4db7fbadf47f0e9ccf1ba2fc2153cb
Кирий Семён <kirii@sfedu.ru>
2024-06-30 19:02:28 +0300

    trash remove logic

---
+++ tilemap/trash_tile.gd
@@ -0,0 +1,47 @@
+extends StaticBody2D
+
+
+var trash_health = 10
+var player
+var can_dig = true
+var field_tile = preload("res://tilemap/field_tile.tscn")
+
+func grow_cattail():
+	$Plant.visible = true
+	for i in range(2):
+		await get_tree().create_timer(2).timeout
+		$Plant.frame += 1
+
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	player = get_tree().get_nodes_in_group("player")[0]
+	$Label.text = str(trash_health)
+	$Sprite2D.flip_h = rng.randi() % 2
+	$Sprite2D.flip_v = rng.randi() % 2
+
+
+func _unhandled_input(_event):
+	if Input.is_action_pressed("left_click"):
+		if $Frame.visible and can_dig:
+			$SuckSound.play()
+			trash_health -= 1
+			$Label.text = str(trash_health)
+			if trash_health == 0:
+				can_dig = false
+				var instance = field_tile.instantiate()
+				get_tree().root.add_child(instance)
+				instance.position = position
+
+				queue_free()
+			get_tree().create_timer(0.2).timeout.connect(func(): can_dig = true)
+
+
+func _on_mouse_entered():
+	if (player.position - position).length() < 128:
+		$Label.visible = true
+		$Frame.visible = true
+
+
+func _on_mouse_exited():
+	$Label.visible = false
+	$Frame.visible = false
