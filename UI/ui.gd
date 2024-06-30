7bff8dceab85ef60733d7116c58c00da4c8c8d43
TheBogdichHD <nottoowellconstructed@gmail.com>
2024-06-30 19:58:26 +0300

    ui reimagining

--- UI/ui.gd
+++ UI/ui.gd
@@ -1,13 +1,26 @@
 extends CanvasLayer
 
 
-@onready var potato_label = $Resources/HBoxContainer/Potato/Label
-@onready var cattail_label = $Resources/HBoxContainer/Cattail/Label
-@onready var spider_plant_label = $Resources/HBoxContainer/SpiderPlant/Label
+@onready var potato_label = $Resources/HBoxContainer2/HBoxContainer/Potato/Label
+@onready var cattail_label = $Resources/HBoxContainer2/HBoxContainer/Cattail/Label
+@onready var spider_plant_label = $Resources/HBoxContainer2/HBoxContainer/SpiderPlant/Label
+@onready var water_label = $Resources/HBoxContainer2/Water/Water
+@onready var time_label = $Resources/HBoxContainer2/Time/Label
+@onready var qouta_bar = $Resources/VBoxContainer/MarginContainer/Qouta
+@onready var qouta_label = $Resources/VBoxContainer/Label
+@onready var timer = $Timer
+
 
 func _ready():
 	GameManager.amount_changed.connect(change_text)
 	change_text()
+	qouta_bar.max_value = GameManager.max_quota_amount
+	qouta_label.text = "Квота картошки: %s" % GameManager.max_quota_amount
+	timer.start(GameManager.max_qouta_time)
+
+
+func _process(_delta):
+	time_label.text = str(int(timer.time_left))
 
 
 func change_text():
@@ -16,8 +29,14 @@ func change_text():
 	spider_plant_label.text = str(GameManager.spiderplant_amount)
 	
 	
-	$Resources/VBoxContainer/Water.text = "%s/%s" % \
+	water_label.text = "%s/%s" % \
 		[
 		GameManager.water_amount,
 		GameManager.max_water_amount
 		]
+
+	qouta_bar.value = GameManager.potato_amount
+
+
+func _on_timer_timeout():
+	pass
