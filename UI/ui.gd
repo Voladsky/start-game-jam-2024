8befd91169e664576ed7a02688b2ff8ef0b06998
TheBogdichHD <nottoowellconstructed@gmail.com>
2024-06-30 14:31:16 +0300

    add ui icons

--- UI/ui.gd
+++ UI/ui.gd
@@ -1,19 +1,20 @@
 extends CanvasLayer
 
+
+@onready var potato_label = $Resources/HBoxContainer/Potato/Label
+@onready var cattail_label = $Resources/HBoxContainer/Cattail/Label
+@onready var spider_plant_label = $Resources/HBoxContainer/SpiderPlant/Label
+
 func _ready():
 	GameManager.amount_changed.connect(change_text)
 	change_text()
 
 
 func change_text():
-	$Resources/ResourceBar.text = "Potatoes: %s ; Cattails: %s ; Spiderplants: %s ;\n
-	Quote: %s" % \
-		[
-		GameManager.potato_amount,
-		GameManager.cattail_amount,
-		GameManager.spiderplant_amount,
-		GameManager.quote_potato_amount,
-		]
+	potato_label.text = str(GameManager.potato_amount)
+	cattail_label.text = str(GameManager.cattail_amount)
+	spider_plant_label.text = str(GameManager.spiderplant_amount)
+	
 	
 	$Resources/VBoxContainer/Water.text = "%s/%s" % \
 		[
