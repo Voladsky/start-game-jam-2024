e70041e638d3f666dc43da1c12108221e8ae6b4c
Владислав Крылов <krylov@sfedu.ru>
2024-07-01 02:27:39 +0300

    Update cattail.gd

--- tilemap/cattail.gd
+++ tilemap/cattail.gd
@@ -3,8 +3,8 @@ extends Sprite2D
 
 func _ready():
 	$Plant.play()
-	if ($Cattail.frame != 2):
+	if (frame != 2):
 		for i in range(2):
 			await get_tree().create_timer(2).timeout
-			$Cattail.frame += 1
+			frame += 1
 			$GrowingParticle.emitting = true
