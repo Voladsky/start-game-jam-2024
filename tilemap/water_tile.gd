427dd1a769235a8bfbbf0845859bd73f54581f9d
TheBogdichHD <nottoowellconstructed@gmail.com>
2024-06-30 13:43:10 +0300

    water collect still not fixed completely

--- tilemap/water_tile.gd
+++ tilemap/water_tile.gd
@@ -3,26 +3,30 @@ extends StaticBody2D
 
 @export var max_water_amount = 20
 var water_amount = max_water_amount
-
-var can_suck = true
 var player
+var field_tile = preload("res://tilemap/field_tile.tscn")
+
 
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
 	$Label.text = str(water_amount)
 
-func _input_event(_viewport, _event, _shape_idx):
+
+func _unhandled_input(_event):
 	if Input.is_action_pressed("left_click"):
-		if (player.position - position).length() < 128:
-			if GameManager.water_amount + 1 <= GameManager.max_water_amount and can_suck:
+		if $Frame.visible:			
+			if GameManager.water_amount + 1 <= GameManager.max_water_amount and GameManager.can_suck:
 				$SuckSound.play()
 				GameManager.change_water_amount(1)
 				water_amount -= 1
 				$Label.text = str(water_amount)
-				can_suck = false
 				if water_amount == 0:
+					var instance = field_tile.instantiate()
+					get_tree().root.add_child(instance)
+					instance.position = position
+					
 					queue_free()
-				get_tree().create_timer(0.2).timeout.connect(func(): can_suck = true)
+				GameManager.suck_water()
 
 
 func _on_mouse_entered():
